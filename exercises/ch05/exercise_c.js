2e1ca6e9a8c3a53aee915f8035173f3a566e2093
Juan Miguel Alvarez Tosco <juan.alvarez@mutuatfe.com>
2022-01-23 19:45:04 +0000

    Chapters 4, 5 exercises resolved

--- exercises/ch05/exercise_c.js
+++ exercises/ch05/exercise_c.js
@@ -1,8 +1,11 @@
 // Refactor `fastestCar` using `compose()` and other functions in pointfree-style.
 
 // fastestCar :: [Car] -> String
-const fastestCar = (cars) => {
-  const sorted = sortBy(car => car.horsepower, cars);
-  const fastest = last(sorted);
-  return concat(fastest.name, ' is the fastest');
-};
+// const fastestCar = (cars) => {
+//   const sorted = sortBy(car => car.horsepower, cars);
+//   const fastest = last(sorted);
+//   return concat(fastest.name, ' is the fastest');
+// };
+
+const isFastestSuffix = n => concat(n, ' is the fastest');
+const fastestCar = compose(isFastestSuffix, prop('name'), last, sortBy(prop('horsepower')));
